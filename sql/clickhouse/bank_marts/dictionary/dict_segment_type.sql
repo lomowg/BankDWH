ceab7e7df0ad8cbc4c10eb5f27034e2821344b21
lomowg <sokol789123@yandex.ru>
2026-04-26 15:48:16 +0300

    Исправлен словарь

--- sql/clickhouse/bank_marts/dictionary/dict_segment_type.sql
+++ sql/clickhouse/bank_marts/dictionary/dict_segment_type.sql
@@ -5,6 +5,9 @@ CREATE DICTIONARY IF NOT EXISTS bank_marts.dict_segment_type
     segment_type_name String
 )
 PRIMARY KEY segment_type_id
-SOURCE(CLICKHOUSE(QUERY 'SELECT segment_type_id, segment_type_code, segment_type_name FROM bank_marts.dim_segment_type'))
+SOURCE(CLICKHOUSE(
+    DB 'bank_marts'
+    TABLE 'dim_segment_type'
+))
 LAYOUT(FLAT())
 LIFETIME(MIN 60 MAX 300);
